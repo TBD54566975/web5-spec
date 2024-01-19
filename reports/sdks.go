14af74683f9cb7c9ae572f5f12cfe532a8619593
Phoebe Lew <plew@squareup.com>
2024-01-19 15:22:44 +1100

    Update regex

--- reports/sdks.go
+++ reports/sdks.go
@@ -36,7 +36,7 @@ var (
 			Repo:         "TBD54566975/tbdex-js",
 			ArtifactName: "junit-results",
 			FeatureRegex: regexp.MustCompile(`TbdexTestVectors(\w+)`),
-			VectorRegex:  regexp.MustCompile(`\w+ \w+ (\w+)`),
+			VectorRegex:  regexp.MustCompile(`.* TbdexTestVectors\w+ (\w+)`),
 			VectorPath:   "test-vectors",
 			Type:         "tbdex",
 		},
